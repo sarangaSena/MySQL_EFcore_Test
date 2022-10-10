337b2dfc49b516eac3327d47198f188cf3b72f87
saranga <saranganirwana@gmail.com>
2022-10-10 10:14:28 +0530

    Add project files.

---
+++ MySQL_EFcore_Test/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MySQL_EFcore_Test.Models;
+
+namespace MySQL_EFcore_Test.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options):base(options)
+        {
+
+        }
+
+        public DbSet<Category>? Categories { get; set; }
+    }
+}
---
+++ MySQL_EFcore_Test/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MySQL_EFcore_Test.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
